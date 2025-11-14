ef04da72d53020fba6ad9f0c9534dc590fbab60b
ConnorNielsen <156475394+ConnorNielsen@users.noreply.github.com>
2025-11-14 16:50:22 -0700

    Added basic files

---
+++ TMParser.java
@@ -0,0 +1,5 @@
+public class TMParser {
+    public TMParser() {
+        
+    }
+}
--- TMSimulator.java
+++ TMSimulator.java
@@ -1,3 +1,6 @@
 public class TMSimulator {
-    
+    public static void main(String[] args) {
+        TMParser parser = new TMParser();
+        TMTranslator translator = new TMTranslator();
+    }
 }
