dd95497826722aed7b169c3c3bfd6e0bbb56c6b0
ConnorNielsen <156475394+ConnorNielsen@users.noreply.github.com>
2025-11-14 17:59:29 -0700

    Changed some logic

--- tm/TMState.java
+++ tm/TMState.java
@@ -13,24 +13,29 @@ public class TMState {
         }
     }
 
-    private Hashtable<String, DestinationSet> transTable;
+    private Hashtable<String, Set<String>> transTable;
     private String name;
     public TMState(String name) {
         this.name = name;
         this.transTable = new Hashtable<>();
     }
 
-    public boolean addTransition(String onSymb, String writeSymb, String direction, String stateName) {
+    public boolean addTransition(String destStateName, String onSymb, String writeSymb, String direction) {
         if (this.transTable.contains(onSymb)) {
             return false;
         }
         
-        this.transTable.put(onSymb, new DestinationSet(writeSymb, direction, stateName));
+        this.transTable.put(onSymb, Set.of(writeSymb, direction, destStateName));
 
         return true;
+    }
+
+    public Set<String> getDestInfo(String onSymb) {
+        return this.transTable.get(onSymb);
     }
 
     public String getName() {
         return this.name;
     }
+
 }
